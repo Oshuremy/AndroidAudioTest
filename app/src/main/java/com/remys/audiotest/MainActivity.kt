036a559fe75d104491bb3e332a4fc21e4532fac0
Remy S <remy.sainmont@gmail.com>
2020-05-11 16:05:13 +0200

    start to add record audio with google doc MediaRecorder

--- app/src/main/java/com/remys/audiotest/MainActivity.kt
+++ app/src/main/java/com/remys/audiotest/MainActivity.kt
@@ -1,48 +1,153 @@
 package com.remys.audiotest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
+private const val LOG_TAG = "AudioRecordTest"
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class MainActivity : AppCompatActivity() {
+    private var fileName: String = ""
+
+    private var mediaRecorder: MediaRecorder? = null
+
     private var mediaPlayer: MediaPlayer? = null
+
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION)
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        else
+            false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         readAudioFile()
+
+        // Record to the external cache directory for visibility
+        fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+        readAudioListener()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaRecorder?.release()
+        mediaRecorder = null
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 
     private fun readAudioFile() {
         mediaPlayer = MediaPlayer.create(this, R.raw.sound_push)
-        mediaPlayer?.setOnPreparedListener {
-            Log.d("#test", "ready")
-        }
 
-        pushButton.setOnTouchListener { _, event ->
-            handleTouch(event)
+        readButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> mediaPlayer?.start()
+                MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                    mediaPlayer?.pause()
+                    mediaPlayer?.seekTo(0)
+                }
+            }
             true
         }
     }
 
-    private fun handleTouch(event: MotionEvent) {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                Log.d("#test", "down")
-                mediaPlayer?.start()
+    private fun readAudioListener() {
+        var mStartRecording = true
+        var mStartPlaying = true
+
+        record.text = "Start recording"
+        record.setOnClickListener {
+            onRecord(mStartRecording)
+            record.text = when (mStartRecording) {
+                true -> "Stop recording"
+                false -> "Start recording"
             }
-            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
-                Log.d("#test", "cancel or up")
-                mediaPlayer?.pause()
-                mediaPlayer?.seekTo(0)
+            mStartRecording = !mStartRecording
+        }
+
+        play.text = "Start playing"
+        record.setOnClickListener {
+            onPlay(mStartPlaying)
+            record.text = when (mStartPlaying) {
+                true -> "Stop playing"
+                false -> "Start playing"
             }
-            else -> {
-                Log.d("#test", "else")
+            mStartPlaying = !mStartPlaying
+        }
+    }
+
+    private fun onRecord(start: Boolean) = if (start)
+        startRecording()
+    else
+        stopRecording()
+
+    private fun onPlay(start: Boolean) = if (start)
+        startPlaying()
+    else
+        stopPlaying()
+
+    private fun startRecording() {
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed : $e")
+            }
+
+            start()
+        }
+    }
+
+    private fun stopRecording() {
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+        mediaRecorder = null
+    }
+
+    private fun startPlaying() {
+        mediaPlayer = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed $e")
             }
         }
+    }
+
+    private fun stopPlaying() {
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
