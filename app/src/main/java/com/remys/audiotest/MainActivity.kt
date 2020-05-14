68277673ccec5a5ac3d4e4fd58902bf7b26e71e3
Alexandre Seneviratne <alexandre.seneviratne@adopteunmec.com>
2020-05-14 12:15:52 +0200

    Draft

--- app/src/main/java/com/remys/audiotest/MainActivity.kt
+++ app/src/main/java/com/remys/audiotest/MainActivity.kt
@@ -2,8 +2,11 @@ package com.remys.audiotest
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
@@ -11,13 +14,16 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.remys.audiotest.SpinnerHelper.BITRATE_SELECTOR
 import com.remys.audiotest.SpinnerHelper.ENCODER_SELECTOR
 import com.remys.audiotest.SpinnerHelper.EXTENSION_SELECTOR
 import com.remys.audiotest.SpinnerHelper.getAudioEncoder
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -25,27 +31,38 @@ import java.util.Locale
 
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    OnAudioFileListener {
     private var bitrateValue: Int = 0
     private lateinit var encoderValue: String
     private lateinit var extensionValue: String
 
     private var mediaRecorder: MediaRecorder? = null
+    private var mediaPlayer: MediaPlayer? = null
 
     // Requesting permission to RECORD_AUDIO
     private var recordPermission: String = Manifest.permission.RECORD_AUDIO
 
     private var isRecording: Boolean = false
+    private var isPlaying: Boolean = false
+
+    private var path: String? = null
+    private var allFiles: ArrayList<File>? = null
+
+    private var playlistAdapter: PlaylistAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        path = this.getExternalFilesDir("/")?.absolutePath
+
         setSpinners()
         initSpinnersListener()
+        setRecyclerView()
 
         record_button.setOnTouchListener { _, event ->
-            if (checkPermissions()){
+            if (checkPermissions()) {
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
                         // Start recording
@@ -70,15 +87,14 @@ class MainActivity : AppCompatActivity() {
         timer.base = SystemClock.elapsedRealtime()
         timer.start()
 
-        val formatter = SimpleDateFormat("MM_dd_hh:mm:ss", Locale.FRANCE)
-        val recordPath: String? = this.getExternalFilesDir("/")?.absolutePath
+        val formatter = SimpleDateFormat("dd:HH:mm:ss", Locale.FRANCE)
         val recordFile = "${bitrateValue}_${encoderValue}_${formatter.format(Date())}$extensionValue"
 
         mediaRecorder = MediaRecorder()
         mediaRecorder?.apply {
             this.setAudioSource(MediaRecorder.AudioSource.MIC)
             this.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
-            this.setOutputFile("$recordPath/$recordFile")
+            this.setOutputFile("$path/$recordFile")
             this.setAudioEncoder(getAudioEncoder(encoderValue))
             this.setAudioEncodingBitRate(bitrateValue)
 
@@ -102,10 +118,11 @@ class MainActivity : AppCompatActivity() {
             this.release()
         }
         mediaRecorder = null
+
         recordText.text = ""
+
+        setRecyclerView()
     }
-
-
 
     private fun checkPermissions(): Boolean {
         return if (ActivityCompat.checkSelfPermission(this, recordPermission) == PackageManager.PERMISSION_GRANTED)
@@ -114,17 +131,14 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(recordPermission), REQUEST_RECORD_AUDIO_PERMISSION)
             false
         }
-
     }
 
     private fun setSpinners() {
         val bitrateSelection = this.resources.getStringArray(R.array.bitrate_select)
         bitrate_selector.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, bitrateSelection)
 
-
         val encoderSelection = this.resources.getStringArray(R.array.encoder_select)
         encoder_selector.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, encoderSelection)
-
 
         val extensionSelection = this.resources.getStringArray(R.array.extension_select)
         extension_selector.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, extensionSelection)
@@ -133,8 +147,10 @@ class MainActivity : AppCompatActivity() {
     private fun initSpinnersListener() {
         bitrate_selector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                bitrateValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, BITRATE_SELECTOR, position).toInt()
+                bitrateValue =
+                    SpinnerHelper.getSpinnerSelectedValue(applicationContext, BITRATE_SELECTOR, position).toInt()
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 bitrateValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, BITRATE_SELECTOR, 0).toInt()
             }
@@ -144,6 +160,7 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 encoderValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, ENCODER_SELECTOR, position)
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 encoderValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, ENCODER_SELECTOR, 0)
             }
@@ -154,10 +171,91 @@ class MainActivity : AppCompatActivity() {
                 extensionValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, EXTENSION_SELECTOR,
                     position)
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 extensionValue = SpinnerHelper.getSpinnerSelectedValue(applicationContext, EXTENSION_SELECTOR, 0)
             }
         }
     }
 
+    private fun setRecyclerView() {
+        val directory = File(path)
+        allFiles = directory.listFiles()?.toMutableList() as ArrayList<File>?
+
+        playlistAdapter = PlaylistAdapter(allFiles, this)
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = LinearLayoutManager(this.applicationContext)
+
+        recycler_view.adapter = playlistAdapter
+
+        playlistAdapter?.notifyDataSetChanged()
+    }
+
+    private fun playAudio(file: File?, imageView: ImageView) {
+        mediaPlayer = MediaPlayer()
+
+        Log.d("AlexTest", file!!.absolutePath)
+
+        mediaPlayer?.apply {
+            /*setAudioAttributes(AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                .build())*/
+
+            try {
+                setOnPreparedListener {
+                    MediaPlayer.OnPreparedListener { mediaPlayer?.start() }
+                }
+                setDataSource(file.path)
+                prepare()
+            } catch (e: IOException) {
+                Log.e("AlexTest", "prepare() failed $e")
+            }
+
+            start()
+        }
+
+
+        imageView.setImageDrawable(resources.getDrawable(R.drawable.list_pause_btn))
+
+        isPlaying = true
+
+        mediaPlayer?.setOnCompletionListener {
+            MediaPlayer.OnCompletionListener {
+                it?.apply {
+                    stop()
+                    release()
+
+                }
+
+                imageView.setImageDrawable(resources.getDrawable(R.drawable.list_play_btn))
+                isPlaying = false
+                mediaPlayer = null
+            }
+
+        }
+    }
+
+    private fun stopAudio(imageView: ImageView) {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+
+        imageView.setImageDrawable(resources.getDrawable(R.drawable.list_play_btn))
+        isPlaying = false
+    }
+
+    override fun playAudioFile(file: File?, imageView: ImageView) {
+        if (!isPlaying) {
+            playAudio(file, imageView)
+        } else {
+            stopAudio(imageView)
+        }
+    }
+
+    override fun getAudioFileDetail(position: Int) {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/remys/audiotest/PlaylistAdapter.kt
+++ app/src/main/java/com/remys/audiotest/PlaylistAdapter.kt
@@ -3,20 +3,30 @@ package com.remys.audiotest
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.io.File
 
-class PlaylistAdapter(private val record: List<String>?): RecyclerView.Adapter<PlaylistAdapter.Viewholder>() {
+class PlaylistAdapter(private val files: ArrayList<File>?, val listener: OnAudioFileListener):
+    RecyclerView.Adapter<PlaylistAdapter.Viewholder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_record, parent, false)
         return Viewholder(itemView);
     }
 
-    override fun getItemCount(): Int = record?.size ?: 0
+    override fun getItemCount(): Int = files?.size ?: 0
 
     override fun onBindViewHolder(holder: Viewholder, position: Int) {
+        holder.recordTitle.text = files?.get(position)?.name ?: ""
+
+        holder.playButton.setOnClickListener {
+            listener.playAudioFile(files?.get(position), holder.playButton)
+        }
     }
 
     class Viewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val recordTitle: TextView = itemView.findViewById(R.id.record_title)
+        val playButton: ImageView = itemView.findViewById(R.id.play_button)
     }
 }
